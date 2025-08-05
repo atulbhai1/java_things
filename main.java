7b458a8447f4b5ff401ecabcc533ac2724a4c762
atulbhai1 <atulbhai.nitin@gmail.com>
2025-08-05 17:43:58 -0400

    made stuff

--- main.java
+++ main.java
@@ -1,8 +1,20 @@
-public class Hello {
-    public static void main(String a[]) {
-        int num = 7%9;
-        num += 2;
-        System.out.println(num);
+public class main {
+    public static void main(String[] args){
+
+        String day = "Satundy";
+
+        day = switch(day) {
+            case "Monday" -> "Sunday";
+
+            case "Saturday", "Sunday" -> "Alarm set to 6";
+
+            default -> "Alarm set to 7";
+
+        };
+
+        System.out.println(day);
+
+
 
     }
 }
