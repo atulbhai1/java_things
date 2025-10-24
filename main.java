118c76203252e0e610f35a3a552ea007eeb7b650
atulbhai1 <atulbhai.nitin@gmail.com>
2025-10-24 18:06:10 -0400

    made stuff

--- main.java
+++ main.java
@@ -1,9 +1,14 @@
 public class main {
     public static void main(String a[]){
-        int nums[][] = new int[4][2];
+        int nums[][] = new int[4][];
+
+        nums[0] = new int[3];
+        nums[1] = new int[2];
+        nums[2] = new int[4];
+        nums[3] = new int[5];
         System.out.println();
-        for (int i=0; i<=3; i++){
-            for (int j=0; j<=1; j++){
+        for (int i=0; i<nums.length; i++){
+            for (int j=0; j<nums[i].length; j++){
                 int temp = (int)(Math.random()*100);
                 nums[i][j] = temp;
                 //System.out.print(nums[i][j]+" ");
