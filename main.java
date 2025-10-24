b2fe4bf1b3d35071fe1c8ef0428095fd1e0fd654
atulbhai1 <atulbhai.nitin@gmail.com>
2025-10-24 17:50:34 -0400

    made stuff

--- main.java
+++ main.java
@@ -1,24 +1,15 @@
-class Computer {
-    public void playMusic(){
-        System.out.println("MOOSIC");
-    }
-    public String sendText(String message, long phoneNumber) {
-        System.out.println("Sending Text: "+message+" to "+phoneNumber);
-        return (String) ("Successfully sent message to "+ phoneNumber);
-    }
-    public int add(int n1, int n2, int n3){
-        return n1+n2+n3;
-    }
-    public int add(int n1, int n2){
-        return n1+n2;
-    }
-}
-
 public class main {
     public static void main(String a[]){
-        Computer comp = new Computer();
-        int r1 = comp.add(2, 3, 4);
-        System.out.println(r1);
+        int nums[] = new int[4];
+        System.out.println(nums[1]);
+        System.out.println();
+        nums[1] = 6;
+        nums[0] = 2;
+        nums[2] = 9;
+        nums[3] = 5;
+        for (int i=0; i<=3; i++){
+            System.out.println(nums[i]);
+        }
     }
 
 }
