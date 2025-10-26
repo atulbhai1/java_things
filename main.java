f970ba9bfa44d7043e2e6605baa82d951e9d474b
atulbhai1 <atulbhai.nitin@gmail.com>
2025-10-26 19:00:21 -0400

    made stuff

--- main.java
+++ main.java
@@ -1,31 +1,10 @@
-class Student{
-    int rollin;
-    String name;
-    int mark;
-}
-
 public class main {
     public static void main(String a[]){
-        Student s1 = new Student();
-        s1.name = "Atul";
-        s1.mark = 88;
-        s1.rollin = 1;
-        Student s2 = new Student();
-        s2.name = "Bart";
-        s2.mark = 78;
-        s2.rollin = 2;
-        Student s3 = new Student();
-        s3.name = "Harsh";
-        s3.mark = 98;
-        s3.rollin = 3;
-        Student students[] = new Student[3];
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
-        for (Student student_temp: students){
-            System.out.println(student_temp.rollin+". "+student_temp.name+":"+student_temp.mark);
-        }
-
+        StringBuffer sb = new StringBuffer("hello");
+        sb.append(" world");
+        sb.deleteCharAt(2);
+        sb.insert(2, "I");
+        System.out.println(sb.substring());
     }
 
 }
