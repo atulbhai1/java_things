eba7830284b1225bba7b891cc6de8d4a059aa705
atulbhai1 <atulbhai.nitin@gmail.com>
2025-10-29 15:47:34 -0400

    made stuff

--- main.java
+++ main.java
@@ -1,10 +1,29 @@
+class Mobile {
+    String brand;
+    int price;
+    String network;
+    static String name;
+    public void show(){
+        System.out.println(brand+" : "+price+" : "+name);
+    }
+}
+
 public class main {
     public static void main(String a[]){
-        StringBuffer sb = new StringBuffer("hello");
-        sb.append(" world");
-        sb.deleteCharAt(2);
-        sb.insert(2, "I");
-        System.out.println(sb.substring());
+        Mobile mobile1 = new Mobile();
+        mobile1.brand = "Apple";
+        mobile1.price = 1500;
+        mobile1.name = "SmartPhone";
+
+        Mobile mobile2 = new Mobile();
+        mobile2.brand = "Samsung";
+        mobile2.price = 1700;
+        mobile2.name = "Phone";
+
+        Mobile.name = "Smart Phone";
+        mobile1.show();
+        mobile2.show();
+        System.out.println(Mobile.name);
     }
 
 }
