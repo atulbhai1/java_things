56dd73a2d02f4f6b066ed1d4bb10557c82b7f7a5
atulbhai1 <atulbhai.nitin@gmail.com>
2025-10-29 16:08:53 -0400

    made stuff

--- main.java
+++ main.java
@@ -3,31 +3,35 @@ class Mobile {
     int price;
     String network;
     static String name;
+    static {
+        name = "Phone";
+        System.out.println("In STATIC");
+    }
+    public Mobile(){
+        brand = "";
+        price = 200;
+    }
     public void show(){
         System.out.println(brand+" : "+price+" : "+name);
     }
-    public static void show1(Mobile obj){
-        System.out.println(obj.brand+" : "+obj.price+" : "+name);
-    }
+
 }
 
 public class main {
-    public static void main(String a[]){
-        Mobile mobile1 = new Mobile();
-        mobile1.brand = "Apple";
-        mobile1.price = 1500;
-        mobile1.name = "SmartPhone";
+    public static void main(String a[]) throws ClassNotFoundException
+    {
+        //Mobile mobile1 = new Mobile();
+        //mobile1.brand = "Apple";
+        //mobile1.price = 1500;
+        //mobile1.name = "SmartPhone";
 
-        Mobile mobile2 = new Mobile();
-        mobile2.brand = "Samsung";
-        mobile2.price = 1700;
-        mobile2.name = "Phone";
+        //Mobile mobile2 = new Mobile();
 
-        Mobile.name = "Smart Phone";
         //mobile1.show();
         //mobile2.show();
-        //System.out.println(Mobile.name);
-        Mobile.show1(mobile1);
+        Class.forName("Mobile");
+
+
     }
 
 }
