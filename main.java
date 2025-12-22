19f7f2274f4de5bea42ed730def01afac660dc31
atulbhai1 <atulbhai.nitin@gmail.com>
2025-12-22 13:34:18 -0500

    made stuff

--- main.java
+++ main.java
@@ -1,30 +1,8 @@
-class Human {
-
-    int age;
-    String name;
-    public Human(){
-        age = 12;
-        name = "bob";
-    }
-    public Human(int a, String n){
-        age = a;
-        name = n;
-    }
-
-    public void display(){
-        System.out.println(age+":"+name);
-    }
-
-}
 
 public class main {
     public static void main(String a[])
     {
-
-        Human obj = new Human();
-        Human obj2 = new Human(18, "atul");
-        obj.display();
-        obj2.display();
+        calculator obj = new calculator();
 
     }
 
