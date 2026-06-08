d816fa800443a6fe376cd44b75566f3e2c637131
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-08 16:58:29 -0400

    Practiced Java

--- main.java
+++ main.java
@@ -1,8 +1,22 @@
+abstract class Car{
+    public abstract void drive();
+    public void playMusic(){
+        System.out.println("Playing music");
+    }
 
+}
+
+class WagonR extends Car{
+    public void drive(){
+        System.out.println("Driving WagonR");
+    }
+}
 public class main {
     public static void main(String a[])
     {
-        calculator obj = new calculator();
+        Car obj = new WagonR();
+        obj.drive();
+        obj.playMusic();
 
     }
 
