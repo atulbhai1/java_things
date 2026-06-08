e1a744e1f04d806571df7c2cbf318fc9722894d5
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-08 17:18:14 -0400

    Practiced Java

--- main.java
+++ main.java
@@ -1,17 +1,14 @@
-class A{
-    public void show(){
-        System.out.println("in A show");
-    }
+abstract class A{
+    public abstract void show();
 }
 
 
 public class main {
     public static void main(String a[])
     {
-        A obj = new A()
-        {
+        A obj = new A(){
             public void show(){
-                System.out.println("in nAw show");
+                System.out.println("in B show");
             }
         };
         obj.show();
