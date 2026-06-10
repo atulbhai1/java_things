a6315975a623f2f354e32f27b5b9e2bf08a027dc
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-10 13:57:39 -0400

    Practiced Java

--- main.java
+++ main.java
@@ -4,9 +4,35 @@ enum Status{
 public class main {
     public static void main(String a[])
     {
-        Status s = Status.Running;
-        s.ordinal();
-        Status[] values = Status.values();
+        Status s = Status.Pending;
+
+        switch(s){
+            case Running:
+                System.out.println("Running");
+                break;
+            case Failed:
+                System.out.println("Failed");
+                break;
+            case Pending:
+                System.out.println("Pending");
+                break;
+            default:
+                System.out.println("Unknown");
+                break;
+        }
+
+        if (s==Status.Running){
+            System.out.println("All good");
+        }
+        else if (s==Status.Failed){
+            System.out.println("Try Again");
+        }
+        else if (s==Status.Pending){
+            System.out.println("Please wait");
+        }
+        else {
+            System.out.println("Done");
+        }
     }
 
 }
