4fbbe7d612b5dc9c1f6932313c81956d066b80bd
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-11 13:11:18 -0400

    Practiced Java

--- main.java
+++ main.java
@@ -1,27 +1,21 @@
-enum Laptop{
-    Macbook(2000), XPS(2200), Surface, Thinkpad(1800);
+class A {
+    public void showTheDataBelongingToThisClass(){
+        System.out.println("Hello World");
+ }
+}
 
-    public int getPrice() {
-        return price;
-    }
-
-    private int price;
-
-    private Laptop() {
-        this.price = 500;
-    }
-
-    private Laptop(int price) {
-        this.price = price;
+class B extends A{
+    @Override
+    public void showTheDataBelongingToThisClass(){
+        System.out.println("Hello World2");
     }
 }
+
 public class main {
     public static void main(String a[])
     {
-
-        for (Laptop lap: Laptop.values()){
-
-        System.out.println("Laptop: " + lap + "----" + "Price: "+lap.getPrice());}
+        B i = new B();
+        i.showTheDataBelongingToThisClass();
 
     }
 
