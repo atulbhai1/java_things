b6efc21fea0dd264bf2357a8ddbdef812c13ad96
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-12 12:29:53 -0400

    Practiced Java

--- main.java
+++ main.java
@@ -1,14 +1,27 @@
-@FunctionalInterface
-interface A{
-    int add(int b, int c);
-}
-
-
 public class main {
     public static void main(String a[])
     {
-        A obj = (b, c) -> b+c;
+        int i = 0;
+        int j = 0;
+        int nums[] = new int[5];
 
+        try
+        {
+            j = 8 / i;
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Problem Found: "+e);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Problem Found: "+e);
+        }
+        catch(Exception e){
+            System.out.println("Problem Found: "+e);
+        }
+        System.out.println("The value of j is: "+j);
+        System.out.println("Bye");
     }
 
 }
