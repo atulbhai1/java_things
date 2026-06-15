5fc9542feea760bae24bb3de2ff34882ab6b8d91
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-15 13:09:01 -0400

    Practiced Java

--- main.java
+++ main.java
@@ -1,20 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-
 
 public class main {
     public static void main(String a[]) throws IOException
     {
+        int num = 0;
+        //BufferedReader br = null;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));)
+        {
+            //br = new BufferedReader(new InputStreamReader(System.in));
+            num = Integer.parseInt(br.readLine());
+        }
 
-        //InputStreamReader in = new InputStreamReader(System.in);
-        //BufferedReader bf = new BufferedReader(in);
-        Scanner sc = new Scanner(System.in);
-        //System.out.println(Integer.parseInt(bf.readLine()));
-        System.out.println(sc.nextInt());
-        //bf.close();
-        sc.close();
     }
 
 }
