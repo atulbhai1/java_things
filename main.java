149c57fc2435530a4c5a2336c7ce7940fa7e8593
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-16 12:29:40 -0400

    Practiced Java

--- main.java
+++ main.java
@@ -3,6 +3,12 @@ class A extends Thread{
         for (int i=1;i<=10;i++)
         {
         System.out.println("hi");
+        try {
+            Thread.sleep(5);
+        }
+        catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
         }
     }
 }
@@ -12,6 +18,12 @@ class B extends Thread{
         for (int i=1;i<=10;i++)
         {
             System.out.println("hello");
+            try {
+                Thread.sleep(5);
+            }
+            catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
@@ -21,7 +33,15 @@ public class main {
     {
         A obj1 =  new A();
         B obj2 = new B();
+        obj1.setPriority(Thread.MAX_PRIORITY);
+        obj2.setPriority(Thread.MAX_PRIORITY);
         obj1.start();
+        try {
+            Thread.sleep(2);
+        }
+        catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
         obj2.start();
     }
 
