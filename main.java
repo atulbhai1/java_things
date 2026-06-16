4a33f02c04f4a16a44f133e468302e7598077895
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-16 12:39:03 -0400

    Practiced Java

--- main.java
+++ main.java
@@ -1,48 +1,34 @@
-class A extends Thread{
-    public void run(){
-        for (int i=1;i<=10;i++)
-        {
-        System.out.println("hi");
-        try {
-            Thread.sleep(5);
-        }
-        catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-        }
-    }
-}
-
-class B extends Thread{
-    public void run(){
-        for (int i=1;i<=10;i++)
-        {
-            System.out.println("hello");
-            try {
-                Thread.sleep(5);
-            }
-            catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-}
-
 public class main {
     public static void main(String a[])
     {
-        A obj1 =  new A();
-        B obj2 = new B();
-        obj1.setPriority(Thread.MAX_PRIORITY);
-        obj2.setPriority(Thread.MAX_PRIORITY);
-        obj1.start();
-        try {
-            Thread.sleep(2);
-        }
-        catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-        obj2.start();
+        Runnable obj1 =  () -> {
+                for (int i=1;i<=10;i++)
+                {
+                    System.out.println("hi");
+                    try {
+                        Thread.sleep(5);
+                    }
+                    catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            };
+        Runnable obj2 = () -> {
+                for (int i=1;i<=10;i++)
+                {
+                    System.out.println("hello");
+                    try {
+                        Thread.sleep(5);
+                    }
+                    catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            };
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        t1.start();
+        t2.start();
     }
 
 }
