142bafac559b0855615ae38155aa1e274d850a60
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-18 16:20:58 -0500

    Practiced Java

--- main.java
+++ main.java
@@ -1,34 +1,32 @@
+class Counter{
+    int count;
+    public synchronized void increment(){
+        count++;
+    }
+}
+
 public class main {
-    public static void main(String a[])
-    {
+    public static void main(String a[]) throws InterruptedException {
+        Counter c = new Counter();
         Runnable obj1 =  () -> {
-                for (int i=1;i<=10;i++)
+                for (int i=1;i<=10000;i++)
                 {
-                    System.out.println("hi");
-                    try {
-                        Thread.sleep(5);
-                    }
-                    catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
+                    c.increment();
                 }
             };
         Runnable obj2 = () -> {
-                for (int i=1;i<=10;i++)
+                for (int i=1;i<=10000;i++)
                 {
-                    System.out.println("hello");
-                    try {
-                        Thread.sleep(5);
-                    }
-                    catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
+                    c.increment();
                 }
             };
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
         t1.start();
         t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(c.count);
     }
 
 }
