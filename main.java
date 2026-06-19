d1c43d3de67c175c4d69144da11df7a0231d31da
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-19 13:38:02 -0500

    Practiced Java

--- main.java
+++ main.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList;
+import java.util.Set;
 import java.util.Collection;
-import java.util.List;
+import java.util.TreeSet;
 
 public class main {
     public static void main(String a[]) throws InterruptedException {
-        List<Integer> nums = new ArrayList<Integer>();
+        Set<Integer> nums = new TreeSet<Integer>();
         nums.add(6);
         nums.add(8);
         nums.add(3);
-        nums.add(2);
+        nums.add(8);
 
 
-        System.out.println(nums.indexOf(8));
 
         for (int n : nums){
             System.out.println(n);
