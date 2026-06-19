09336089d7d2890c9887b29b57f579bcbd5aef43
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-19 13:50:12 -0500

    Practiced Java

--- main.java
+++ main.java
@@ -1,19 +1,19 @@
-import java.util.Set;
-import java.util.Collection;
-import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class main {
-    public static void main(String a[]) throws InterruptedException {
-        Set<Integer> nums = new TreeSet<Integer>();
-        nums.add(6);
-        nums.add(8);
-        nums.add(3);
-        nums.add(8);
-
-
-
-        for (int n : nums){
-            System.out.println(n);
+    public static void main(String a[]) {
+        Map<String, Integer> Students = new HashMap<String, Integer>();
+        Students.put("Navin", 56);
+        Students.put("Harsh", 23);
+        Students.put("Sushil", 67);
+        Students.put("Kiran", 92);
+        for (String student: Students.keySet()) {
+            if (Students.get(student) < 70){
+                System.out.println(student+" has failed.");
+            }
         }
     }
 
