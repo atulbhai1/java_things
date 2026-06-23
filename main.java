5edcf7d59db012728d5e94aa359e7c7844e889d8
atulbhai1 <atulbhai.nitin@gmail.com>
2026-06-23 13:31:49 -0400

    Practiced Java

--- main.java
+++ main.java
@@ -1,18 +1,15 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class main {
     public static void main(String a[]) {
         List<Integer> nums = Arrays.asList(7, 5, 75, 2, 9);
-        //Stream<Integer> stream = nums.stream();
-        int sum = 0;
-        for (int i=0;i<nums.size();i++){
-            if (nums.get(i)%2==0) {
-                sum += 2*(nums.get(i));
-            }
-        }
+        Consumer<Integer> con = n -> System.out.println(n);
+
+        nums.forEach(con);
         nums.forEach(n -> System.out.println(n));
     }
 
